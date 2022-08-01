604eb7590591cd82e8c3b523b043fc408f42a883
Raymond <raymond0huang@gmail.com>
2022-08-01 22:22:36 +0800

    Initial Commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by 黄磊 on 2022/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by 黄磊 on 2022/8/1.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
